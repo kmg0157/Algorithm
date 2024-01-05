261b60c70ccaf63b31c59fd8b6eee61c989471d8
kmg0157 <102772772+kmg0157@users.noreply.github.com>
2024-01-05 11:05:12 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> arr, int idx) {
+    int answer = 0;
+    
+    for(int i=idx;i<arr.size();i++)
+    {
+        if(arr[i]==1)
+        {   
+            answer=i;
+            break;
+        }else
+            answer=-1;
+    }
+    
+    return answer;
+}
