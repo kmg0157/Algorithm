efd1ef35a81d0be4ec31a088062935b1bbce3517
kmg0157 <102772772+kmg0157@users.noreply.github.com>
2024-01-11 19:08:08 +0900

    [level Lv.0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    for(auto c:myString)
+    {
+        answer+=tolower(c);
+    }
+    
+    return answer;
+}
