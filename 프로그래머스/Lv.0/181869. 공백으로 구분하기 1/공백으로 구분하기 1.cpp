d3769196ed6c9200b55baea8022941709d115ff0
kmg0157 <102772772+kmg0157@users.noreply.github.com>
2024-01-13 18:38:44 +0900

    [level Lv.0] Title: 공백으로 구분하기 1, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    string tmp="";
+    for(int i=0;i<my_string.length();i++)
+    {
+        if(my_string[i]==' ')
+        {
+            answer.push_back(tmp);
+            tmp = "";
+        }
+        else
+        {
+            tmp+=my_string[i];
+        }
+    }
+    if (!tmp.empty()) {
+        answer.push_back(tmp);
+    }
+
+    
+    return answer;
+}
