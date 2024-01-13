604fbaaa41b4ee4acd3a1b850e9f6ad11b9e0577
kmg0157 <102772772+kmg0157@users.noreply.github.com>
2024-01-13 18:02:40 +0900

    [level Lv.0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    for(char c:myString)
+    {
+        if(c=='a'||c=='A')
+            answer+='A';
+        else
+        {
+            answer+=tolower(c);
+        }
+    }
+    
+    return answer;
+}
