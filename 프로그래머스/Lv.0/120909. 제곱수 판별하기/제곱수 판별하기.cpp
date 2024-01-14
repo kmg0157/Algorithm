8a4239d767e1ac1ff9a88bab837319a41a12b66c
kmg0157 <102772772+kmg0157@users.noreply.github.com>
2024-01-14 20:57:57 +0900

    [level Lv.0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i=1;i*i<=n;i++)
+    {
+        if(i*i==n)
+            answer=1;
+        else
+            answer=2;
+    }
+    
+    return answer;
+}
