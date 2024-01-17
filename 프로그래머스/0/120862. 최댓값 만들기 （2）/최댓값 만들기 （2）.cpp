329391a5d9f8844bf2a488d979825f298091fc00
kmg0157 <102772772+kmg0157@users.noreply.github.com>
2024-01-17 23:03:49 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end(), greater<int>());
+    
+    int n = numbers.size();
+    
+    // 가장 큰 두 양수끼리 곱하기
+    int positiveMax = numbers[0] * numbers[1];
+    
+    // 가장 작은 두 음수끼리 곱하기
+    int negativeMax = numbers[n - 1] * numbers[n - 2];
+    
+    // 양수끼리 또는 음수끼리 곱했을 때 더 큰 값을 선택
+    return max(positiveMax, negativeMax);
+}
