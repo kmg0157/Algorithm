8a87d68ab18cd1aa8c94a66ac9f09fa2fde81d76
kmg0157 <102772772+kmg0157@users.noreply.github.com>
2024-02-14 12:43:28 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int a=x;
+    int sum=0;
+    
+    while(x>0)
+    {
+        sum+=x%10;
+        x/=10;
+    }
+    if(a%sum!=0) answer=false;
+    
+    return answer;
+}
