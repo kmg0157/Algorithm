8545c8a892c6bd4508ace5fe5fbbe39abf1b411b
kmg0157 <102772772+kmg0157@users.noreply.github.com>
2024-08-14 23:54:48 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = []
+    
+    for char in my_string:
+        if char.isdigit():
+            answer.append(int(char))
+    
+    answer.sort()
+    return answer
